e8f0e499401909997f9ae4a24c305f1127acfe8a
Chandra Kiran G <luna@ChandraranGsAir.alphionsee.in>
2023-09-23 12:26:48 +0530

    chore: initial push

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![allow(unused)]
+
+use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(Parser)]
+struct CLI {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = CLI::parse();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
+
+    Ok(())
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    grrs::find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
+}
